e9e35cc3255065bdd524e9a56ee86c1b2e026698
scotchwsplenda <lockwood.oliver@gmail.com>
2021-02-06 15:55:53 -0800

    accuracy view updated

--- season_prediction_dj/seahawks_2022_predictions/views.py
+++ season_prediction_dj/seahawks_2022_predictions/views.py
@@ -31,62 +31,62 @@ def hawks_scores_view(request):
 # https://stackoverflow.com/questions/39109045/numpy-where-with-multiple-conditions -> answer #2
 #  sqlite3.connect must have '../' before db file in order to navigate up one directory when running in terminal, but NOT for view in browser
 def accurate_wl(request):
-    # table for predicted spread
+    # table for pred spread
     df_predictions = pd.read_sql('SELECT * FROM vPredicted_Score_Dif', sqlite3.connect('db.sqlite3'))
     funky = get_df_panda()
     # table for difference between predicted and actual spread
     df_accurate_wl = pd.DataFrame()
     df_accurate_wl['Name'] = df_predictions['username']
     df_accurate_wl['WEEK1'] = np.where(
-        ((df_predictions['Predicted_Wk1_Spred'] * funky['Differenchy'][0]) > 0), # correctly predicted W/L 
-         (df_predictions['Predicted_Wk1_Spred'] - funky['Differenchy'][0]).abs(),'WRONG') # dif between actual and predicted, (if W/L wrong -1)
+        ((df_predictions['Pred_Wk1_Spred'] * funky['Differenchy'][0]) > 0), # correctly predicted W/L 
+         (df_predictions['Pred_Wk1_Spred'] - funky['Differenchy'][0]).abs(),'WRONG') # dif between actual and predicted, (if W/L wrong -1)
     df_accurate_wl['WEEK2'] = np.where(
-        ((df_predictions['Predicted_Wk2_Spred'] * funky['Differenchy'][1]) > 0), 
-         (df_predictions['Predicted_Wk2_Spred'] - funky['Differenchy'][1]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk2_Spred'] * funky['Differenchy'][1]) > 0), 
+         (df_predictions['Pred_Wk2_Spred'] - funky['Differenchy'][1]).abs(),'WRONG') 
     df_accurate_wl['WEEK3'] = np.where(
-        ((df_predictions['Predicted_Wk3_Spred'] * funky['Differenchy'][2]) > 0), 
-         (df_predictions['Predicted_Wk3_Spred'] - funky['Differenchy'][2]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk3_Spred'] * funky['Differenchy'][2]) > 0), 
+         (df_predictions['Pred_Wk3_Spred'] - funky['Differenchy'][2]).abs(),'WRONG') 
     df_accurate_wl['WEEK4'] = np.where(
-        ((df_predictions['Predicted_Wk4_Spred'] * funky['Differenchy'][3]) > 0),
-         (df_predictions['Predicted_Wk4_Spred'] - funky['Differenchy'][3]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk4_Spred'] * funky['Differenchy'][3]) > 0),
+         (df_predictions['Pred_Wk4_Spred'] - funky['Differenchy'][3]).abs(),'WRONG') 
     df_accurate_wl['WEEK5'] = np.where(
-        ((df_predictions['Predicted_Wk5_Spred'] * funky['Differenchy'][4]) > 0), 
-         (df_predictions['Predicted_Wk5_Spred'] - funky['Differenchy'][4]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk5_Spred'] * funky['Differenchy'][4]) > 0), 
+         (df_predictions['Pred_Wk5_Spred'] - funky['Differenchy'][4]).abs(),'WRONG') 
     df_accurate_wl['WEEK6'] = np.where(
-        ((df_predictions['Predicted_Wk6_Spred'] * funky['Differenchy'][5]) > 0), 
-         (df_predictions['Predicted_Wk6_Spred'] - funky['Differenchy'][5]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk6_Spred'] * funky['Differenchy'][5]) > 0), 
+         (df_predictions['Pred_Wk6_Spred'] - funky['Differenchy'][5]).abs(),'WRONG') 
     df_accurate_wl['WEEK7'] = np.where(
-        ((df_predictions['Predicted_Wk7_Spred'] * funky['Differenchy'][6]) > 0), 
-         (df_predictions['Predicted_Wk7_Spred'] - funky['Differenchy'][6]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk7_Spred'] * funky['Differenchy'][6]) > 0), 
+         (df_predictions['Pred_Wk7_Spred'] - funky['Differenchy'][6]).abs(),'WRONG') 
     df_accurate_wl['WEEK8'] = np.where(
-        ((df_predictions['Predicted_Wk8_Spred'] * funky['Differenchy'][7]) > 0),
-         (df_predictions['Predicted_Wk8_Spred'] - funky['Differenchy'][7]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk8_Spred'] * funky['Differenchy'][7]) > 0),
+         (df_predictions['Pred_Wk8_Spred'] - funky['Differenchy'][7]).abs(),'WRONG') 
     df_accurate_wl['WEEK9'] = np.where(
-        ((df_predictions['Predicted_Wk9_Spred'] * funky['Differenchy'][8]) > 0), 
-         (df_predictions['Predicted_Wk9_Spred'] - funky['Differenchy'][8]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk9_Spred'] * funky['Differenchy'][8]) > 0), 
+         (df_predictions['Pred_Wk9_Spred'] - funky['Differenchy'][8]).abs(),'WRONG') 
     df_accurate_wl['WEEK10'] = np.where(
-        ((df_predictions['Predicted_Wk10_Spred'] * funky['Differenchy'][9]) > 0), 
-         (df_predictions['Predicted_Wk10_Spred'] - funky['Differenchy'][9]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk10_Spred'] * funky['Differenchy'][9]) > 0), 
+         (df_predictions['Pred_Wk10_Spred'] - funky['Differenchy'][9]).abs(),'WRONG') 
     df_accurate_wl['WEEK11'] = np.where(
-        ((df_predictions['Predicted_Wk11_Spred'] * funky['Differenchy'][10]) > 0), 
-         (df_predictions['Predicted_Wk11_Spred'] - funky['Differenchy'][10]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk11_Spred'] * funky['Differenchy'][10]) > 0), 
+         (df_predictions['Pred_Wk11_Spred'] - funky['Differenchy'][10]).abs(),'WRONG') 
     df_accurate_wl['WEEK12'] = np.where(
-        ((df_predictions['Predicted_Wk12_Spred'] * funky['Differenchy'][11]) > 0),
-         (df_predictions['Predicted_Wk12_Spred'] - funky['Differenchy'][11]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk12_Spred'] * funky['Differenchy'][11]) > 0),
+         (df_predictions['Pred_Wk12_Spred'] - funky['Differenchy'][11]).abs(),'WRONG') 
     df_accurate_wl['WEEK13'] = np.where(
-        ((df_predictions['Predicted_Wk13_Spred'] * funky['Differenchy'][12]) > 0), 
-         (df_predictions['Predicted_Wk13_Spred'] - funky['Differenchy'][12]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk13_Spred'] * funky['Differenchy'][12]) > 0), 
+         (df_predictions['Pred_Wk13_Spred'] - funky['Differenchy'][12]).abs(),'WRONG') 
     df_accurate_wl['WEEK14'] = np.where(
-        ((df_predictions['Predicted_Wk14_Spred'] * funky['Differenchy'][13]) > 0), 
-         (df_predictions['Predicted_Wk14_Spred'] - funky['Differenchy'][13]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk14_Spred'] * funky['Differenchy'][13]) > 0), 
+         (df_predictions['Pred_Wk14_Spred'] - funky['Differenchy'][13]).abs(),'WRONG') 
     df_accurate_wl['WEEK15'] = np.where(
-        ((df_predictions['Predicted_Wk15_Spred'] * funky['Differenchy'][14]) > 0), 
-         (df_predictions['Predicted_Wk15_Spred'] - funky['Differenchy'][14]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk15_Spred'] * funky['Differenchy'][14]) > 0), 
+         (df_predictions['Pred_Wk15_Spred'] - funky['Differenchy'][14]).abs(),'WRONG') 
     df_accurate_wl['WEEK16'] = np.where(
-        ((df_predictions['Predicted_Wk16_Spred'] * funky['Differenchy'][15]) > 0),
-         (df_predictions['Predicted_Wk16_Spred'] - funky['Differenchy'][15]).abs(),'WRONG') 
+        ((df_predictions['Pred_Wk16_Spred'] * funky['Differenchy'][15]) > 0),
+         (df_predictions['Pred_Wk16_Spred'] - funky['Differenchy'][15]).abs(),'WRONG') 
     df_accurate_wl['WEEK17'] = np.where(
-        ((df_predictions['Predicted_Wk17_Spred'] * funky['Differenchy'][16]) > 0), 
-         (df_predictions['Predicted_Wk17_Spred'] - funky['Differenchy'][16]).abs(),'WRONG')
+        ((df_predictions['Pred_Wk17_Spred'] * funky['Differenchy'][16]) > 0), 
+         (df_predictions['Pred_Wk17_Spred'] - funky['Differenchy'][16]).abs(),'WRONG')
     return render(request, "seahawks_2022_predictions/accuracy.html", {'df_accurate_wl': df_accurate_wl.to_html(), 'df_predictions': df_predictions.to_html() })
 
