90323fb7e001c41cee6a63ec82f7384bc8876d1b
scotchwsplenda <lockwood.oliver@gmail.com>
2021-02-17 11:49:50 -0800

    populating df_accurate_wl with a loop

--- season_prediction_dj/seahawks_2022_predictions/prediction_merge.py
+++ season_prediction_dj/seahawks_2022_predictions/prediction_merge.py
@@ -6,31 +6,54 @@ import numpy as np
 from hawksscore import get_df_panda
 
 
-# loc is by name iloc is by idex
-oppscore = get_df_panda().T
-
 dd = get_df_panda().reset_index(drop = True)
 dd = dd.loc[:16,['Title','Differenchy']]
-
-
-
 df_predictions = pd.read_sql('SELECT * FROM vPredicted_Score_Dif', sqlite3.connect('../db.sqlite3'))
 df_predictions = df_predictions.T
-# make columns equal
 new_header = df_predictions.iloc[0] #grab the first row for the header
 df_predictions = df_predictions[1:] #take the data less the header row
 df_predictions.columns = new_header #set the header row as the df header
 df_predictions = df_predictions.reset_index(drop = True)
-print(df_predictions.shape)
-print(dd.shape)
-print(df_predictions)
-print(dd)
-
-# bigdata = pd.concat([dd, df_predictions], axis=0, ignore_index=True)
-# print(bigdata)
-
 bigdata = pd.merge(dd, df_predictions, left_index=True, right_index=True)
-print(bigdata)
-
-# bigdata = dd.insert(df_predictions,)
+bigdata = bigdata.fillna(0)
+bigdata['Differenchy'] = bigdata['Differenchy'].astype(int)
 # print(bigdata)
+
+
+# print(np.subtract(bigdata['Differenchy'], bigdata['Ken']))
+# https://stackoverflow.com/questions/26333005/numpy-subtract-every-row-of-matrix-by-vector/26333184
+v = bigdata['Differenchy'][:, np.newaxis]
+# print(type(v))
+
+# for x in v:
+#     print(type(x))
+
+# print(bigdata.iloc[:,1:])
+# print(bigdata.iloc[:,1:]-v)
+c = bigdata['Differenchy']
+for x in c:
+    print(x)
+# print(type(c))
+
+dumn = []
+for index, row in bigdata.iterrows():
+    dumn.append(np.where((row[1] * row[2:]) > 0, (row[1] - row[2:]).abs(), None))
+print(dumn)
+
+
+    # print(type(index))
+    # print(index)
+    # print('~~~~~~')
+
+    # print(type(row))
+    # print(row)
+    # print('------')
+
+
+    # print(np.where((column * v) > 0, (column - v).abs(), None))
+
+'''
+df_accurate_wl['WEEK1'] = np.where(
+    ((df_predictions['Pred_Wk1_Spred'] * funky['Differenchy'][0]) > 0), # correctly predicted W/L 
+        (df_predictions['Pred_Wk1_Spred'] - funky['Differenchy'][0]).abs(),None) # dif between actual and predicted, (if W/L wrong -1)
+'''
